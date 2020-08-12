9ac80608675b86994d3041cde697b7064e37f532
beata <beata@emc2wave.com>
2020-08-12 15:38:21 -0700

    add removal container if children list is =0

--- app.js
+++ app.js
@@ -76,6 +76,9 @@ const clearItems = () => {
 const deleteItem = (e) => {
   const element = e.currentTarget.parentElement.parentElement;
   list.removeChild(element);
+  if (list.children.length === 0) {
+    container.classList.remove('show-container');
+  }
 };
 
 const editItem = () => {
